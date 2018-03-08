b18713808a6ae1968af670b0cecdce00398bc202
neuJump <35955724+neuJump@users.noreply.github.com>
2018-03-08 20:53:00 +0000

    Testing second command 'turn on'

--- tests/test_COMP30670_Software_Engineering_Assignment_3.py
+++ tests/test_COMP30670_Software_Engineering_Assignment_3.py
@@ -30,11 +30,33 @@ class TestComp30670_software_engineering_assignment_3(unittest.TestCase):
         inputFile = "./data/input_test.txt"
         size, instructions, type = cli.inputParse(inputFile)
         assert size == 4
-        assert instructions == ['turn off 660,55 through 986,197\n', '\n', 'turn off 341,304 through 638,850\n', '\n', 'turn off 199,133 through 461,193\n', '\n', 'switch 322,558 through 977,958']
+        assert instructions == ['turn off 660,55 through 986,197\n', '\n', 'turn on 160,45 through 786,457\n', '\n', 'turn off 341,304 through 638,850\n', '\n', 'turn off 199,133 through 461,193\n', '\n', 'switch 322,558 through 977,958']
         
         # Command test 1
         inputFile = "./data/input_test.txt"
         size, instructions, type = cli.inputParse(inputFile)
         instruction1 = instructions[0]
-        cmd = instruction1[0:8] 
-        assert cmd == "turn off"
+        cmd1 = instruction1[0:8] 
+        assert cmd1 == "turn off"
+        
+        # Command test 2
+        inputFile = "./data/input_test.txt"
+        size, instructions, type = cli.inputParse(inputFile)
+        instruction2 = instructions[2]
+        cmd2 = instruction2[0:7]
+        assert cmd2 == "turn on"
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
