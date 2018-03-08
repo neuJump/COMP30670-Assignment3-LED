77d2c970287d2b1baeaf7a3269b6b286acd941ac
neuJump <35955724+neuJump@users.noreply.github.com>
2018-03-08 21:29:32 +0000

    Created three basic methods: control(), transCheck() and apply()

--- COMP30670_Software_Engineering_Assignment_3/lights.py
+++ COMP30670_Software_Engineering_Assignment_3/lights.py
@@ -11,11 +11,24 @@ class LightTest:
     
     lights = None;
 
-    #def __init__(self, size):
-    def listTest(size):
+    def __init__(self, size):
 
-        #self.size = size
-        lights = np.array([[0]*size for _ in range(size)])
-        return lights
+        self.size = size
+        self.lights = np.array([[0]*self.size for _ in range(self.size)])
+        
+    def control(self, data, start):
+        t1 = data[start:].rsplit(sep=" ", maxsplit=2)
+        t2 = t1[0].rsplit(sep=",", maxsplit=1)
+        t3 = t1[2].rsplit(sep=",", maxsplit=1)
+        
+    def transCheck(self, dataSet, transType):
+        
+        # Regular expression
+        regEx = ".*(turn on|turn off|switch)\s*([+-]?\d+)\s*,\s*([+-]?\d+)\s*through\s*([+-]?\d+)\s*,\s*([+-]?\d+).*"
+    
+    def apply(self, data, lineCount, transType):
+          
+        # Extract string  
+        input, result = self.transCheck(data[lineCount], transType);
         
    
